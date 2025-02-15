55ec6885789e852340a7a1d2e23b0140003490a7
olathedev <olapademicheal3@gmail.com>
2025-02-15 13:14:03 +0100

    complete createSfae lock function

--- script/SafeLock.sol
+++ script/SafeLock.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import './lib/Errors.sol';
+import './lib/Events.sol';
+
 
 contract TimeVest {
     struct Lock {
+        uint256 id;
         string title;
-        uint256 amout;
+        uint256 amount;
         uint256 duration;
     }
 
@@ -14,6 +18,22 @@ contract TimeVest {
     mapping(address => Lock) safeLocks;
 
     function createSafeLock(string memory _title, uint256 _durationInDays) external payable {
-        
+        if(msg.value < 0) revert Errors.InvalidDepositAmount();
+        if(_durationInDays < 10) revert Errors.InvalidLockDuration();
+
+        uint256 timestamp = block.timestamp + (_durationInDays * 1 days);
+
+        safeLocks[msg.sender] = Lock({
+            id: _locksIdCounter + 1,
+            title: _title,
+            amount: msg.value,
+            duration: timestamp
+        });
+
+        emit Events.safeLockCreated(msg.sender, _title, msg.value, timestamp);
     }
+
+    
+
+
 }
--- script/lib/Events.sol
+++ script/lib/Events.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.13;
 
 
-library Errors {
+library Events {
     event safeLockCreated(address indexed user, string indexed title, uint256 indexed amount, uint256 unlockTime);
     event Withdrawn(address indexed user, uint256 amount);
 }
